9d9f6aba22c420d1250861e292ad51828a9db5c8
yelahyelah <55163751+yelahyelah@users.noreply.github.com>
2019-09-11 11:35:29 -0400

    Add files via upload
    Mining - Part 1

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	int notification = 0; 
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Time.time > 3 && notification == 0) {
+	   print ("It's been three seconds!");
+	   notification = 1;
+	   }
+	   
+    }
+}
